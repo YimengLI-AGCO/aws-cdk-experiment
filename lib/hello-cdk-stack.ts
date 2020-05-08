6089bbf224f27c887c464a4ea5f9b0f88bfc5d25
YimengLI-AGCO <yimeng.li@agcocorp.com>
2020-05-07 21:55:28 -0500

    add stack and construct

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from '@aws-cdk/core';
+import sqs = require('@aws-cdk/aws-sqs');
+import { QueueRecorder } from './queue-recorder';
+
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const queue = new sqs.Queue(this, 'HelloQueue');
+
+    new QueueRecorder(this, 'QueueRecorder', {inputQueue: queue});
   }
 }
---
+++ lib/queue-recorder.ts
@@ -0,0 +1,32 @@
+import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
+import event_sources = require('@aws-cdk/aws-lambda-event-sources');
+import sqs = require('@aws-cdk/aws-sqs');
+import dynamodb = require('@aws-cdk/aws-dynamodb')
+
+
+export interface QueueRecorderProps {
+  inputQueue: sqs.Queue
+}
+
+export class QueueRecorder extends cdk.Construct {
+  constructor(parent: cdk.Construct, id: string, props: QueueRecorderProps) {
+    super(parent, id);
+
+    const fn = new lambda.Function(this, 'HelloFunction', {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      code: lambda.Code.asset('lambda'),
+      handler: 'index.handler'
+    });
+
+    fn.addEventSource(new event_sources.SqsEventSource(props.inputQueue));
+
+    const table = new dynamodb.Table(this, 'QueueRecorderTable', {
+      partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING}
+    });
+
+    fn.addEnvironment('TABLE_NAME', table.tableName);
+
+    table.grantWriteData(fn);
+  }
+}
