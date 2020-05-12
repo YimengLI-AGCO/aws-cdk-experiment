d54bfb14b814680889607a20d847331667e0db5e
YimengLI-AGCO <yimeng.li@agcocorp.com>
2020-05-12 16:10:26 -0500

    deploy jar file to s3

--- lib/status-service.ts
+++ lib/status-service.ts
@@ -4,6 +4,8 @@ import dynamodb = require('@aws-cdk/aws-dynamodb');
 import apigateway = require('@aws-cdk/aws-apigateway');
 import { Duration } from '@aws-cdk/core';
 import bucket = require('@aws-cdk/aws-s3');
+import deploy = require('@aws-cdk/aws-s3-deployment');
+
 
 export interface StatusServiceProps {
   dynamoTable: dynamodb.Table
@@ -13,8 +15,13 @@ export class StatusService extends cdk.Construct {
   constructor(parent: cdk.Construct, id: string, props: StatusServiceProps) {
     super(parent, id);
 
+
     const s3bucket = new bucket.Bucket(this, 'Hello S3', {
       bucketName: 'yimengs3testtest'
+    });
+    new deploy.BucketDeployment(this, 'Deploy', {
+      destinationBucket: s3bucket,
+      sources: [deploy.Source.asset('./java-lambda/target/jarfile/')]
     });
 
     const getAllData = new lambda.Function(this, '[DYNAMO]getAllData', {
@@ -45,7 +52,7 @@ export class StatusService extends cdk.Construct {
 
     const createIntegration = new apigateway.LambdaIntegration(createData);
     items.addMethod('POST', createIntegration);
-
+    
     props.dynamoTable.grantReadData(getAllData);
     props.dynamoTable.grantWriteData(createData);
   }
