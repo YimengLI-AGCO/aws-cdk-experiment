7b8bb0aa83ccad39b23d1250402df0e6a0fae40f
YimengLI-AGCO <yimeng.li@agcocorp.com>
2020-05-12 16:09:30 -0500

    Update index.py

--- lambda/index.py
+++ lambda/index.py
@@ -3,23 +3,33 @@ import boto3
 import json
 import os
 import uuid
+import traceback
 
 def handler(event, context):
-  print("HELLO PYTHON")
-  dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
-  table = dynamodb.Table(os.environ['TABLE_NAME'])
+  try:
+    print("HELLO PYTHON")
+    dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
+    table = dynamodb.Table(os.environ['TABLE_NAME'])
 
-  message = os.environ['TABLE_NAME']
-  print(message)
+    message = os.environ['TABLE_NAME']
+    print(message)
 
-  str_id = str(uuid.uuid1())
-  response = table.put_item(
-    Item = {
-      'id': str(str_id),
-      'yimeng-test': '[WRITE FROM PYTHON]{}'.format(message)
-    }
-  )
+    str_id = str(uuid.uuid1())
+    response = table.put_item(
+      Item = {
+        'id': str(str_id),
+        'yimeng-test': '[WRITE FROM PYTHON]{}'.format("HAHAHAHAHAHAHA")
+      }
+    )
 
-  return {
-    'message': message
-  }
+    return {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": "OK",
+        "isBase64Encoded": False
+      }
+  except Exception as e:
+    print(e)
+    traceback.print_exc()
